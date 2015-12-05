fd09e736adc8bb539943e5ecbfc8bf8ff3a5c089
Manash Taunk <maannaash@gmail.com>
2015-12-04 21:11:26 -0600

    Updated to create initial version

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -11,11 +11,72 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var scrollView: UIScrollView!
+    
+    
+    @IBOutlet var username: UITextField!
+    @IBOutlet var password: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var signUpButton: UIButton!
+    var signupActive = true
+    
+    
+    func displayAlert(title:String, Message:String){
+        
+        var alert = UIAlertController(title: title, message: Message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { (action) -> Void in
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    @IBAction func SignUpButtonPressed(sender: UIButton) {
+        
+        if username.text == "" || password.text == "" {
+            
+            displayAlert("Error in Form", Message: "Please enter a username and password")
+            
+        }else{
+            
+            //Sign up Code
+            
+        }
+        
+        
+    }
+    
+    @IBAction func LoginButton(sender: AnyObject) {
+       
+        
+        if signupActive == true {
+            
+            signUpButton.setTitle("Login", forState: UIControlState.Normal)
+            loginButton.setTitle("New User ? Sign Up", forState: UIControlState.Normal)
+            signupActive = false
+            
+        }else{
+            signUpButton.setTitle("Sign Up", forState: UIControlState.Normal)
+            loginButton.setTitle("Already Registered ? Login", forState: UIControlState.Normal)
+            signupActive = true
 
+        }
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.contentSize.height = 600
         // Do any additional setup after loading the view, typically from a nib.
-        print ("test")
+        
     
     }
 
